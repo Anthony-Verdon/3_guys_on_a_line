c21dd941b2234c04f098d4bf5cdec85cfac72d5f
averdon <anthonyverdon10@gmail.com>
2023-10-14 19:13:59 +0200

    added a visualisation to see the wall before creating it

--- Scripts/createWall.gd
+++ Scripts/createWall.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 const MOUSE_BUTTON_LEFT = 1;
 const WALL_SCENE = preload("res://Scenes/Prefabs/wall.tscn")
+const WALL_TEXTURE = preload("res://Assets/Wall/wall_fill.png")
+const TEXTURE_LENGTH = 32
 
 var firstVertex: Vector2 = Vector2.ZERO
 var wallCount: int = 0
+var newWall = null
+var collisionShape2D = null
+var sprite2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,25 +26,35 @@ func checkMouseClick(event):
 	if (event is InputEventMouseButton
 	&& event.button_index == MOUSE_BUTTON_LEFT
 	&& event.pressed):
-		if (firstVertex == Vector2.ZERO):
-			firstVertex = event.position
+		if (newWall == null):
+			firstVertex = event.position			
+			initWall()
 		else:
-			var secondVertex: Vector2 = event.position
-			initWall(firstVertex, secondVertex)
-			firstVertex = Vector2.ZERO
-
-func initWall(firstVertex: Vector2, secondVertex: Vector2):
-	var vector: Vector2 = secondVertex - firstVertex
-	var newWall = WALL_SCENE.instantiate()
-	var collisionShape2D = newWall.get_child(0)
-	var sprite2D = newWall.get_child(1)
-	newWall.name = "Wall_" + str(wallCount)
-	wallCount += 1
+			createWall(event.position)
+			newWall = null
+	elif (event is InputEventMouseMotion && newWall != null):
+		updateWallBlueprint(event.position)
+	
+func initWall():
+	newWall = WALL_SCENE.instantiate()
+	collisionShape2D = newWall.get_child(0)
+	sprite2D = newWall.get_child(1)
 	collisionShape2D.shape = SegmentShape2D.new()
-	collisionShape2D.shape.a = firstVertex
+	collisionShape2D.shape.a = Vector2(-1, -1)
+	collisionShape2D.shape.b = Vector2(-1, -1)
+	sprite2D.position = firstVertex
+	sprite2D.rotation = Vector2.ZERO.angle()
+	sprite2D.scale.x = Vector2.ZERO.length()
+	add_child(newWall)
+
+func createWall(secondVertex: Vector2):
+	sprite2D.texture = WALL_TEXTURE
+	collisionShape2D.shape.a = firstVertex	
 	collisionShape2D.shape.b = secondVertex
+
+func updateWallBlueprint(secondVertex: Vector2):
+	var vector: Vector2 = secondVertex - firstVertex
 	sprite2D.position = (firstVertex + secondVertex) / 2
 	sprite2D.rotation = vector.angle()
 	#6000 is an arbitrary value found by testing 
-	sprite2D.scale.x = vector.length() / 6000
-	add_child(newWall)
+	sprite2D.scale.x = vector.length() / TEXTURE_LENGTH
